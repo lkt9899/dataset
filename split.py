c85fcf613ea3cf2656c901fd94775b685bc5ceb5
lkt9899 <lkt9899@naver.com>
2021-10-28 00:17:19 +0900

    edit path from on jetson to on colab

--- split.py
+++ split.py
@@ -2,8 +2,8 @@ from glob import glob
 from sklearn.model_selection import train_test_split
 import yaml
 
-img_list = glob('/home/aa/dataset/export/images/*.jpg')
-txt_list = glob('/home/aa/dataset/export/labels/*.txt')
+img_list = glob('/content/dataset/export/images/*.jpg')
+txt_list = glob('/content/dataset/export/labels/*.txt')
 
 print(len(img_list))
 print(len(txt_list))
@@ -12,21 +12,22 @@ train_img_list, val_img_list = train_test_split(img_list, test_size = 0.2, rando
 
 print(len(train_img_list), len(val_img_list))
 
-with open('/home/aa/dataset/export/train.txt', 'w') as f :
+with open('/content/dataset/export/train.txt', 'w') as f :
 	f.write('\n'.join(train_img_list) + '\n')
 
-with open('/home/aa/dataset/export/val.txt', 'w') as f :
+with open('/content/dataset/export/val.txt', 'w') as f :
 	f.write('\n'.join(val_img_list) + '\n')
 
-with open('/home/aa/dataset/data.yaml', 'r') as f :
+with open('/content/dataset/data.yaml', 'r') as f :
 	data = yaml.safe_load(f)
 
 print(data)
 
-data['train'] = '/home/aa/dataset/export/train.txt'
-data['val'] = '/home/aa/dataset/export/val.txt'
+data['train'] = '/content/dataset/export/train.txt'
+data['val'] = '/content/dataset/export/val.txt'
 
-with open('/home/aa/dataset/data.yaml', 'w') as f :
+with open('/content/dataset/data.yaml', 'w') as f :
 	yaml.dump(data, f)
 
 print(data)
+
